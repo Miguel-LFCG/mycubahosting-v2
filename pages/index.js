899be80573f56b19296ab7d90d7809bbc4418d2e
Victor <victor.tendron@epita.fr>
2025-08-18 21:24:00 +0100

    Add whitespace for formatting consistency in the Home component

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       <main>
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          
         </h1>
 
         <p className="description">
